00a1acf1444f4a2042db3862b2355c12eca1f87f
buckwheatthedog <nsteigmann@cca.edu>
2016-09-21 09:17:45 -0700

    Update Problem5.js

--- Problem5.js
+++ Problem5.js
@@ -1,18 +1,22 @@
-
 function setup() {
 
   var y = 0;
   createCanvas(600, 600);
   background(0);
-  for (var x = 25; x < width; x = x + 25) {
-  ellipse(x, y + 25, 25, 25);
+  for (var x = 25; x <= width; x = x + 25) {
+    if (x < width / 2) {
+      for (y = 25; y < x; y = y + 25) {
+        ellipse(x, y, 25, 25);
+      }
+    } else {
+      for (y = 25; y < width - x; y = y + 25) {
+        ellipse(x, y, 25, 25);
+        println(x, y);
+        //ellipse(y, y + 25, 25, 25);
+        //     //  if (y == width / 2) {
 
-    for (var i = 25; i < width; i = i + 25) {
-      ellipse(i, i + 25, 25, 25);
-      if (i == width / 2) {
-        ellipse(i, i - 25, 25, 25);
-        ellipse(i, i - 24, 25, 25);
       }
     }
   }
+}
 }
