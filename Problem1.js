0396c57691bf42d7c4fbb16d5491cc7f57164a57
buckwheatthedog <nsteigmann@cca.edu>
2016-09-20 22:14:19 -0700

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -33,3 +33,17 @@ function setup() {
     i = i + 1;
   }    
 }
+
+
+// or, alteratively you could use the bottom code:
+
+//function setup() {
+//  createCanvas(400, 400);
+//  background(0);
+
+//  for (var x = 10; x < width; x = x + 23) {
+//    for (var y = 10; y < height; y = y + 23) {
+//      ellipse(x, y,23 ,23);
+//    }
+//  }
+//}
